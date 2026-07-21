ac61a726a3ba445fdd913acfe8dc6264c62cffb3
firmansyah0909 <frrmansyy@gmail.com>
2026-07-22 02:29:57 +0700

    Remove pnpm workspace

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
- import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+import App from "./app/App";
+import "./styles/index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
